7c225abbc54d22d4f115633692eda18dcc1ff618
Matthew Mower <matt@theartofnavigation.co.uk>
2023-07-20 12:15:43 +0100

    Here I need help

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-// The virtual_file library supports creating a virtual file that appears to
-// be a contiguous line-based file but which may be represented by multiple
-// backing files on disk
+// virtual_file
 //
-
-// a virtual file is a series of backing files, sections that represent a mapping of line numbers
+// a virtual file represents a contiguous text file that is, behind the
+// scenes, composed of sections of, potentially many, other text files
 //
+// primary use case is for implementing "include" functionality allowing
+// the contents of arbitrary files to be inserted into the text
 //
 
 use std::fs;
-// use std::str;
+use std::ops::Range;
 
 #[derive(Debug)]
 struct BackingFile {
@@ -17,30 +17,56 @@ struct BackingFile {
     size: usize,
 }
 
-// struct Section<'a> {
-//     backing: &'a BackingFile,
-//     indices: (u32, u32),
-// }
+impl BackingFile {
+    pub fn new_from_file(file_name: &str) -> BackingFile {
+        let content = fs::read_to_string(file_name).expect("Unable to read file!");
+        let lines: Vec<String> = content.lines().map(|s: &str| String::from(s)).collect();
+        let count = lines.len();
+        BackingFile {
+            name: String::from(file_name),
+            lines: lines,
+            size: count,
+        }
+    }
+}
 
-// struct VirtualFile {
-//     sections: Vec<Section>,
-// }
+#[derive(Debug)]
+struct Section<'a> {
+    backing: &'a BackingFile,
+    slice: Range<usize>,
+}
 
-fn new_from_file(name: &str) -> BackingFile {
-    let content = fs::read_to_string(name).expect("Unable to read file!");
-    let lines: Vec<String> = content.lines().map(|s: &str| String::from(s)).collect();
-    let count = lines.len();
-    BackingFile {
-        name: String::from(name),
-        lines: lines,
-        size: count,
+impl<'a> Section<'a> {
+    pub fn new_from_backing_file(backing_file: &BackingFile) -> Section {
+        Section {
+            backing: backing_file,
+            slice: 0..backing_file.size,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct VirtualFile<'a> {
+    name: String,
+    backing_files: Vec<BackingFile>,
+    sections: Vec<Section<'a>>,
+}
+
+impl<'a> VirtualFile<'a> {
+    pub fn new_from_file(file_name: &str) -> VirtualFile {
+        let backing_file = BackingFile::new_from_file(file_name);
+        let section = Section::new_from_backing_file(&backing_file);
+
+        VirtualFile {
+            name: String::from(file_name),
+            backing_files: vec![backing_file],
+            sections: vec![section],
+        }
     }
 }
 
 fn main() {
-    let backing_file = new_from_file("dtrh.rez");
+    let virtual_file = VirtualFile::new_from_file("dtrh.rez");
 
-    println!("Backing file: {}", backing_file.name);
-    println!("Contents: {:?}", backing_file.lines);
-    println!("Length: {}", backing_file.size);
+    println!("Backing file: {:?}", virtual_file.name);
 }
